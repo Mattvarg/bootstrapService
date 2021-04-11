f4797962f6f5a18090c9f5113eb4b3db3d83c947
Mathew Varghese <varg.matt@gmail.com>
2021-04-11 17:05:54 -0400

    First commit

---
+++ src/main/java/com/ms1/bootstrapService/BootstrapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms1.bootstrapService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms1/bootstrapService/BootstrapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms1.bootstrapService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
